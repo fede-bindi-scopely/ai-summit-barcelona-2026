2a7128bc10c65ba49df7e73b04099963990fb228
fede-bindi-scopely <federico.bindi@scopely.com>
2026-09-22 10:22:20 +0200

    Revert "task 1: naked loop"
    This reverts commit e0e26bd2654e409bdc9ebd6702da52740e857171.

--- src/handlers.js
+++ src/handlers.js
@@ -24,11 +24,3 @@ export function completeTask(req, res, id) {
   res.writeHead(200, { "Content-Type": "application/json" });
   res.end(JSON.stringify(task));
 }
-
-export function updateTitle(req, res, id, body) {
-  const parsed = JSON.parse(body);
-  const task = store.find(Number(id));
-  task.title = parsed.title;
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(task));
-}
